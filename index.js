e7c9036983786477c8922a136e644398da063690
halim-torayev <halim-torayev@users.noreply.github.com>
2026-01-12 22:03:33 +0300

    loops for myLeads

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-let myLeads = []
+let myLeads = ['www.github.com', 'www.instagram.com', 'www.facebook.com']
 let inputEl = document.getElementById("input-el")
 let inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener('click', function(){
     myLeads.push(inputEl.value)
-    
+
 })  
+
+for (let i = 0; i < myLeads.length; i++){
+    console.log(myLeads[i] + " ")
+}
