cc7ade36b92ac2062f18f7397fd9f654fdd52265
halim-torayev <halim-torayev@users.noreply.github.com>
2026-01-17 17:00:36 +0300

    added some advanced features for listItems

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let myLeads = ['www.github.com', 'www.instagram.com', 'www.facebook.com']
-let inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
-let ulEl = document.getElementById("ul-el")
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener('click', function(){
     myLeads.push(inputEl.value)
 
 })  
 
+let listItems = ""
 for (let i = 0; i < myLeads.length; i++){
-    ulEl.textContent += myLeads[i] + "\n"
-}
+    listItems += '<li>' + myLeads[i] + '</li>'
+
+    //this way of implementing is more advanced and more prefered!!!
+    // const li = document.createElement('li')
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+} 
+ulEl.innerHTML += listItems
+
+
